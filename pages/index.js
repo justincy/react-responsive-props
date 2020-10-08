693528010f8b52b6c1181046b99caa7f09a03a61
Justin <justincyork@gmail.com>
2020-10-08 16:04:12 -0600

    Update index.js to match new pages

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,31 @@
+import Link from "next/link";
 import useBreakpoints from "../lib/useBreakpoints.js";
-import Button from "../lib/Button.js";
-import ButtonResponsive from "../lib/ButtonResponsive.js";
-import ButtonResponsiveHOC from "../lib/ButtonResponsiveHOC.js";
-import Responsive from "../lib/Responsive.js";
-
-const responsiveValues = {
-  xs: {
-    size: "small",
-    color: "#B0E0E6"
-  },
-  sm: {
-    size: "small",
-    color: "#2E8B57"
-  },
-  md: {
-    size: "medium",
-    color: "#F5DEB3"
-  },
-  lg: {
-    size: "large",
-    color: "#F08080"
-  }
-};
 
 export default function IndexPage() {
-  const { active: activeBreakpoint } = useBreakpoints();
+  const breakpoints = useBreakpoints();
   return (
     <div>
-      <p>Active breakpoint: {activeBreakpoint}.</p>
+      <h1>Responsive Prop Patterns in React</h1>
+      <ul>
+        <li>
+          <Link href="conditional">
+            <a>Conditional Rendering</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="object">
+            <a>Object of Props</a>
+          </Link>
+        </li>
+      </ul>
       <p>
-        <Button size="large">Large</Button> <Button>Medium</Button>{" "}
-        <Button size="small">Small</Button>
+        At the foundation of all of these reponsive prop patterns are two hooks:
+        <code>useBreakpoints()</code> and <code>useMediaQuery()</code>.{" "}
+        <code>useBreakpoints()</code> calls <code>useMediaQuery()</code> once
+        for each breakpoint and returns the object printed below. Resize the
+        screen to watch the values change.
       </p>
-      <h3>Responsive Buttons</h3>
-      <p>
-        <Responsive props={responsiveValues}>
-          {({ size, color }) => (
-            <Button size={size} color={color}>
-              &lt;Responsive&gt;
-            </Button>
-          )}
-        </Responsive>
-      </p>
-      <p>
-        <ButtonResponsive
-          size={{ xs: "small", sm: "small", md: "medium", lg: "large" }}
-          color={{ xs: "#B0E0E6", sm: "#2E8B57", md: "#F5DEB3", lg: "#F08080" }}
-        >
-          ButtonResponsive
-        </ButtonResponsive>
-      </p>
-      <p>
-        <ButtonResponsiveHOC
-          size={{ xs: "small", sm: "small", md: "medium", lg: "large" }}
-          color={{ xs: "#B0E0E6", sm: "#2E8B57", md: "#F5DEB3", lg: "#F08080" }}
-        >
-          ButtonResponsiveHOC
-        </ButtonResponsiveHOC>
-      </p>
+      <pre>{JSON.stringify(breakpoints, null, 2)}</pre>
     </div>
   );
 }
