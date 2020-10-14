66b1efc60302da95f5f017f03da9acb3d3752e16
Justin York <jyork@genward.com>
2020-10-14 15:54:07 -0600

    Breakpoint: dynamic hook impl

--- pages/breakpoint.js
+++ pages/breakpoint.js
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import ButtonNaive from "../lib/breakpoint/ButtonNaive.js";
 import ButtonDynamic from "../lib/breakpoint/ButtonDynamic.js";
+import ButtonDynamicHook from "../lib/breakpoint/ButtonDynamicHook.js";
 import ButtonHOC from "../lib/breakpoint/ButtonHOC.js";
 
 export default function ArrayPage() {
@@ -10,42 +11,62 @@ export default function ArrayPage() {
         <a>Home</a>
       </Link>
       <h1>Breakpoint Props</h1>
-      <ButtonNaive
-        sizeXs="small"
-        sizeSm="small"
-        sizeMd="medium"
-        sizeLg="large"
-        colorXs="#B0E0E6"
-        colorSm="#2E8B57"
-        colorMd="#F5DEB3"
-        colorLg="#F08080"
-      >
-        Naive Impl
-      </ButtonNaive>
-      <ButtonDynamic
-        sizeXs="small"
-        sizeSm="small"
-        sizeMd="medium"
-        sizeLg="large"
-        colorXs="#B0E0E6"
-        colorSm="#2E8B57"
-        colorMd="#F5DEB3"
-        colorLg="#F08080"
-      >
-        Dynamic Impl
-      </ButtonDynamic>
-      <ButtonHOC
-        sizeXs="small"
-        sizeSm="small"
-        sizeMd="medium"
-        sizeLg="large"
-        colorXs="#B0E0E6"
-        colorSm="#2E8B57"
-        colorMd="#F5DEB3"
-        colorLg="#F08080"
-      >
-        HOC Impl
-      </ButtonHOC>
+      <p>
+        <ButtonNaive
+          sizeXs="small"
+          sizeSm="small"
+          sizeMd="medium"
+          sizeLg="large"
+          colorXs="#B0E0E6"
+          colorSm="#2E8B57"
+          colorMd="#F5DEB3"
+          colorLg="#F08080"
+        >
+          Naive Impl
+        </ButtonNaive>
+      </p>
+      <p>
+        <ButtonDynamic
+          sizeXs="small"
+          sizeSm="small"
+          sizeMd="medium"
+          sizeLg="large"
+          colorXs="#B0E0E6"
+          colorSm="#2E8B57"
+          colorMd="#F5DEB3"
+          colorLg="#F08080"
+        >
+          Dynamic Impl
+        </ButtonDynamic>
+      </p>
+      <p>
+        <ButtonDynamicHook
+          sizeXs="small"
+          sizeSm="small"
+          sizeMd="medium"
+          sizeLg="large"
+          colorXs="#B0E0E6"
+          colorSm="#2E8B57"
+          colorMd="#F5DEB3"
+          colorLg="#F08080"
+        >
+          Dynamic Hook Impl
+        </ButtonDynamicHook>
+      </p>
+      <p>
+        <ButtonHOC
+          sizeXs="small"
+          sizeSm="small"
+          sizeMd="medium"
+          sizeLg="large"
+          colorXs="#B0E0E6"
+          colorSm="#2E8B57"
+          colorMd="#F5DEB3"
+          colorLg="#F08080"
+        >
+          HOC Impl
+        </ButtonHOC>
+      </p>
     </div>
   );
 }
